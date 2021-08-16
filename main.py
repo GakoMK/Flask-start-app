3856148490809e1ca96568a94344e2c96eaf5af2
Michael <mkletowski@gmail.com>
2021-08-16 14:21:46 +0200

    admin - insignificant fixes

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from app import app
+from manage import User
+from app import app, db
 import models
 import views
 import admin
+import start
 
-
-if __name__ == '__main__':
-    # Start app  
+if __name__ == '__main__':    
+    start.sample_data_db() # not working
     app.run(host='127.0.0.1', port=5000, debug=True) 
